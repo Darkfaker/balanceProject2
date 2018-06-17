32c4e2cfb2382e9376cd676a45a88249c152f899
Darkfaker <1017449263@qq.com>
2018-06-17 22:01:01 +0800

    http commit

---
+++ tcp/head.h
@@ -0,0 +1,38 @@
+#ifndef _HEAD_H
+#define _HEAD_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <sys/sendfile.h>
+
+#include <vector>
+#include <deque>
+#include <list>
+#include <stack>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <string>
+#include <bitset>
+#include <functional>
+#include <algorithm>
+#include <iterator>
+
+#endif
+
+
